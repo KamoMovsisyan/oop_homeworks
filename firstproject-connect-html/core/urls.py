845e593b1e9dc9c576d8296e7abf10e755861349
KamoMovsisyan <kamomov777@mail.ru>
2021-11-19 21:39:37 +0400

    html connect

---
+++ firstproject-connect-html/core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from core import views
+
+urlpatterns = [
+    path('hello/', views.hello),
+    path('introduction/', views.intro),
+    path('curr_time/', views.time_),
+    path('solution/', views.solution),
+    path('list_task/', views.list_task),
+    path('home/', views.home),
+    path('homework_page/', views.homework)
+]
